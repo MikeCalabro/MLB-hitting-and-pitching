e63a3ab2d56e125c581c28d0ade5aea2f665b28f
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-20 20:36:32 -0400

    slow n steady

--- pitching_to_Trout/app.R
+++ pitching_to_Trout/app.R
@@ -1,13 +1,15 @@
 library(shiny)
 library(tidyverse)
 library(png)
+library(shinythemes)
 
 trout_data <- read_rds("trout_data")
 
 ui <- fluidPage(
-   
+   theme = shinytheme("sandstone"),
    titlePanel("Pitching To Mike Trout"),
-
+   navbarPage("Navbar",
+   tabPanel("All Pitches",
    sidebarLayout(
       sidebarPanel(
          checkboxGroupInput("pitches",
@@ -20,12 +22,6 @@ ui <- fluidPage(
                        "Sinker",
                        "Slider"),
                      selected = "4-Seam Fastball"),
-         sliderInput("year",
-                     "Seasons to Include",
-                     min = 2017,
-                     max = 2020,
-                     value = c(2017, 2020),
-                     sep = ""),
          sliderInput("balls",
                      "Balls in the Count",
                      min = 0,
@@ -36,13 +32,24 @@ ui <- fluidPage(
                      min = 0,
                      max = 2,
                      value = c(0, 2)
-                     )
+                     ),
+         selectInput("geom",
+                     "Select Geom Display",
+                     c("Point: Color = Pitch Type" = "Ppt",
+                       "Point: Color = Pitch Result" = "Ppr",
+                       "Bin: Color = Pitch Count" = "Bpc"),
+                     selected = "Bpc")
       ),
 
       mainPanel(
-         plotOutput("plateView", width = "70%", height = "600px"),
-         img(src="plate.png", width = "55%", height = "80px")
+         plotOutput("plateView", width = "62%", height = "540px"),
+         img(src="plate.png", width = "45%", height = "60px")
       )
+   )
+   ),
+   tabPanel("Batted Balls"
+     
+   )
    )
 )
 
@@ -53,23 +60,31 @@ server <- function(input, output) {
    output$plateView <- renderPlot({
      trout_data %>%
        filter(pitch_name %in% input$pitches,
-              game_year %in% (input$year[1]:input$year[2]),
               balls %in% (input$balls[1]:input$balls[2]),
               strikes %in% (input$strikes[1]:input$strikes[2])) %>%
        ggplot(aes(x = plate_x, y = plate_z)) +
-       geom_bin2d(binwidth = c(0.33, 0.2323)) +
+       {
+         if(input$geom == "Bpc"){
+         geom_bin2d(binwidth = c(0.33, 0.2323))}
+       } +
        geom_segment(aes(x = -0.333, y = mean(sz_top), xend = -0.333, yend = mean(sz_bot)), color = "gray") +
        geom_segment(aes(x = 0.333, y = mean(sz_top), xend = 0.333, yend = mean(sz_bot)), color = "gray") +
        geom_segment(aes(x = -1, y = ((mean(sz_top) - mean(sz_bot))/3) + mean(sz_bot),
                         xend = 1, yend = ((mean(sz_top) - mean(sz_bot))/3) + mean(sz_bot)), color = "gray") +
        geom_segment(aes(x = -1, y = mean(sz_top) - ((mean(sz_top) - mean(sz_bot))/3),
                         xend = 1, yend = mean(sz_top) - ((mean(sz_top) - mean(sz_bot))/3)), color = "gray") +
-       geom_segment(aes(x = -1, y = mean(sz_top), xend = 1, yend = mean(sz_top))) +
-       geom_segment(aes(x = -1, y = mean(sz_bot), xend = 1, yend = mean(sz_bot))) +
+       geom_segment(aes(x = -1, y = mean(sz_top), xend = 1, yend = mean(sz_top)), size = 1.5) +
+       geom_segment(aes(x = -1, y = mean(sz_bot), xend = 1, yend = mean(sz_bot)), size = 1.5) +
        geom_segment(aes(x = -1, y = mean(sz_top), xend = -1, yend = mean(sz_bot))) +
        geom_segment(aes(x = 1, y = mean(sz_top), xend = 1, yend = mean(sz_bot))) +
        geom_segment(aes(x = 1, y = mean(sz_top), xend = 1, yend = mean(sz_bot))) +
-       #geom_point(aes(color = pitch_name), size = 1.5) +
+       {
+         if(input$geom == "Ppt"){
+           geom_point(aes(color = pitch_name), size = 1.5)
+         }else if(input$geom == "Ppr"){
+           geom_point(aes(color = description), size = 2)
+         }
+       } +
        ylim(4.1, 1.03) +
        xlim(-1.67, 1.67) +
        theme(axis.ticks = element_blank(),
