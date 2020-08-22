e49fc82c2d2b8b3ace7cd34f9d1601cb827a00cc
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-22 12:12:16 -0400

    commenting - check

--- pitching_to_Trout/app.R
+++ pitching_to_Trout/app.R
@@ -89,7 +89,7 @@ ui <- fluidPage(
          br(),
          tableOutput("typeTable"),
          br(),
-         tableOutput("resultTable2")
+         tableOutput("resultTable")
       )
    ),
    # New tabPanel results in an entirely new screen when "Batted Balls" is clicked
@@ -117,18 +117,26 @@ server <- function(input, output) {
      val <- c(7, 8, 9, 4, 5, 6, 1, 2, 3, 11, 12, 13, 14)
      num_tab <- data.frame(num_x, num_y, val)
      
+     # Using the data that I downloaded from Statcast using the baseballr package
      trout_data %>%
+       # Renaming some descriptions so they aren't as big on screen (Long names in the key squish the strikezone)
        mutate(description = ifelse(description == "hit_into_play_no_out", "hit",
                                    ifelse(description == "hit_into_play", "in_play_out", description))) %>%
+       # All of this filtering means that only the data chosen from the widgets gets displayed on screen
        filter(pitch_name %in% input$pitches,
               description %in% input$results,
               balls %in% (input$balls[1]:input$balls[2]),
               strikes %in% (input$strikes[1]:input$strikes[2])) %>%
+       # Now the plot can be defined
+       # The x and y arguments within ggplot define the location of all the points that will be plotted
        ggplot(aes(x = plate_x, y = plate_z)) +
+       # Each if statement in this renderPlot defines how the plot should react if certain inputs are altered
+       # This one says that the heat map bins should be shown on screen if that selection is made
        {
          if(input$geom == "Bpc"){
-         geom_bin2d(binwidth = c(0.33, 0.2323))}
+         geom_bin2d(binwidth = c(0.33, 0.2323))} # These numbers make the bins fit nicely in the strikezone
        } +
+       # Each of these geom_segments define the strike zone outline that is shown on screen
        geom_segment(aes(x = -0.333, y = mean(sz_top), xend = -0.333, yend = mean(sz_bot)), color = "gray") +
        geom_segment(aes(x = 0.333, y = mean(sz_top), xend = 0.333, yend = mean(sz_bot)), color = "gray") +
        geom_segment(aes(x = -1, y = ((mean(sz_top) - mean(sz_bot))/3) + mean(sz_bot),
@@ -139,6 +147,7 @@ server <- function(input, output) {
        geom_segment(aes(x = -1, y = mean(sz_bot), xend = 1, yend = mean(sz_bot)), size = 1.5) +
        geom_segment(aes(x = -1, y = mean(sz_top), xend = -1, yend = mean(sz_bot)), size = 1.5) +
        geom_segment(aes(x = 1, y = mean(sz_top), xend = 1, yend = mean(sz_bot)), size = 1.5) +
+       # These ifs are used to switch between the color of the points shown on screen (Could be simpler, should fix)
        {
          if(input$geom == "Ppt"){
            geom_point(aes(fill = pitch_name), shape = 21, size = 3, color = "black", stroke = 0.5)
@@ -146,13 +155,16 @@ server <- function(input, output) {
            geom_point(aes(fill = description), shape = 21, size = 3, color = "black", stroke = 0.5)
          }
        } +
+       # If the checkbox for showing numbers is pressed, this geom_text creates thos numbers
        {
          if(input$nums){
            geom_text(data = num_tab, aes(x = num_x, y = num_y, label = val), size = 8.5,color = "black")
          }
        } +
+       # xlim and ylim define the size of the plot
        ylim(1.03, 4.1) +
        xlim(-1.67, 1.67) +
+       # All of these element_blank()'s make the canvas blank, unlike base ggplot which has axis/grid defaults
        theme(axis.ticks = element_blank(),
              axis.text = element_blank(),
              axis.title = element_blank(),
@@ -160,28 +172,35 @@ server <- function(input, output) {
              panel.background = element_blank())
    })
    
+   # The only difference between the next three tables is what I 'group by' to create the rows
    output$typeTable <- renderTable({
-    table_data <- trout_data %>%
-      mutate(description = ifelse(description == "hit_into_play_no_out", "hit",
-                                  ifelse(description == "hit_into_play", "in_play_out", description))) %>%
+     # Same mutating and filtering as for the plot
+     table_data <- trout_data %>%
+       mutate(description = ifelse(description == "hit_into_play_no_out", "hit",
+                                   ifelse(description == "hit_into_play", "in_play_out", description))) %>%
        filter(pitch_name %in% input$pitches,
               description %in% input$results,
               balls %in% (input$balls[1]:input$balls[2]),
               strikes %in% (input$strikes[1]:input$strikes[2]))
-    table_data <- table_data %>%
+     
+     # Created this new dataframe so that I could add this total row
+     # This total row allows me to divide each group_by column by the total pitches so that I can make the share %s
+     table_data <- table_data %>%
        mutate(total = nrow(table_data))
     
+    # This code creates the data in the table
     table_data %>%
-       group_by(pitch_name, total) %>%
-       summarise(count = n()) %>%
-       mutate(share = count/total) %>%
-       select(pitch_name, count, share) %>%
-       arrange(desc(count))
+       group_by(pitch_name, total) %>%      # Create buckets of rows defined by pitch_name 
+       summarise(count = n()) %>%           # Create a column count which says how many observations there are for each group
+       mutate(share = count/total) %>%      # Add the share column - the % of all pitches
+       select(pitch_name, count, share) %>% # Selects the three columns to show
+       arrange(desc(count))                 # Arranges the table with the highest counts at the top
    },
-   striped = TRUE,
-   bordered = TRUE,
-   spacing = "s")
+   striped = TRUE,  # Make the table striped
+   bordered = TRUE, # Makes the table have a border
+   spacing = "s")   # Defines the spacing between rows on the table
    
+   # This tableOutput is the same as typeTable, but is grouped by pitch result (description)
    output$resultTable <- renderTable({
      table_data <- trout_data %>%
        mutate(description = ifelse(description == "hit_into_play_no_out", "hit",
@@ -204,6 +223,7 @@ server <- function(input, output) {
    bordered = TRUE,
    spacing = "s")   
    
+   # This tableOutput is the same as resultTable, but is grouped by zone location (description)
    output$zoneTable <- renderTable({
      table_data <- trout_data %>%
        mutate(description = ifelse(description == "hit_into_play_no_out", "hit",
@@ -229,6 +249,6 @@ server <- function(input, output) {
    
 }
 
-# Run the application 
+# Glues it all together and runs the application 
 shinyApp(ui = ui, server = server)
 
